dae040d06b64bbeefc9494f4c254844d00916a9a
Simona <s.c.dulgheru@gmail.com>
2022-11-05 20:34:10 +0200

    Edit, update, delete skills

--- app/Http/Controllers/Backend/SkillController.php
+++ app/Http/Controllers/Backend/SkillController.php
@@ -6,22 +6,100 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreSkillRequest;
 use App\Models\Skill;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SkillController extends Controller
 {
+
+    /**
+     * Show the skills page.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index() 
     {
         $skills = Skill::all();
         return view('skills.index', compact('skills'));
     }
 
+
+    /**
+     * Create skill and view create page.
+     *
+     * @return \Illuminate\View\View
+     */
     public function create()
     {
         return view('skills.create');
     }
 
+
+    /**
+     * Store a new skill image.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(StoreSkillRequest $request)
     {
-        //TO DO
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('skills');
+
+            Skill::create([
+                'name' => $request->name,
+                'image'=> $image
+            ]);
+
+            return redirect('/skills')->with('status', 'Skill saved successfully!');;
+        }
+        return back();
+    }
+
+
+    /**
+     * Edit skill.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function edit(Skill $skill)
+    {
+        return view('skills.edit', compact('skill'));
+    }
+
+
+   /**
+     * Update the skill name.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Skill $skill)
+    {
+        $request->validate([
+            'name' => ['required', 'min:3'],
+            'image'=> ['nullable', 'image']
+        ]);
+
+        $image = $skill->image;
+        if ($request->hasFile('image')) {
+            Storage::delete($skill->image);
+            $image = $request->file('image')->store('skills');
+        }
+
+        $skill->update([
+            'name' =>$request->name,
+            'image'=>$image
+        ]);
+
+        return redirect('/skills')->with('status', 'Skill updated successfully!');
+    }
+
+
+    public function destroy(Skill $skill)
+    {
+        Storage::delete($skill->image);
+        $skill->delete();
+
+        return back()->with('status', 'Skill deleted successfully!');
     }
 }
--- resources/views/skills/index.blade.php
+++ resources/views/skills/index.blade.php
@@ -1,3 +1,4 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -13,6 +14,20 @@
                    New Skill
                 </a>
             </div>
+            @if (session('status'))
+                <div id="triggerElement" class="flex p-4 mb-4 bg-green-100 rounded-lg dark:bg-green-200" role="alert">
+                    <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-green-700 dark:text-green-800" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Info</span>
+                    <div class="ml-3 text-sm font-medium text-green-700 dark:text-green-800">
+                        {{ session('status') }}
+                    </div>
+                    <button type="button" id="targetElement" class="ml-auto -mx-1.5 -my-1.5 bg-green-100 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex h-8 w-8 dark:bg-green-200 dark:text-green-600 dark:hover:bg-green-300" data-dismiss-target="#triggerElement" aria-label="Close">
+                      <span class="sr-only">Close</span>
+                      <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    </button>
+                </div>      
+            @endif
+
             <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -23,7 +38,7 @@
                             <th scope="col" class="py-3 px-6">
                                 Image
                             </th>
-                            <th scope="col" class="py-3 px-6">
+                            <th scope="col" class="flex justify-end py-2 px-6 mr-6">
                                 Action
                             </th>
                         </tr>
@@ -36,13 +51,22 @@
                                     {{ $skill->name }}
                                 </th>
                                 <td class="py-4 px-6">
-                                    {{ $skill->image }}
+                                    <img src="{{ asset('storage/'. $skill->image) }}" class="w-12 h-12" /> 
                                 </td>
-                                <td class="py-4 px-6">
-                                    <a href="#" 
-                                       class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                                <td class="flex justify-end items-center py-6">
+                                    <a href="{{ route('skills.edit', $skill->id) }}" 
+                                       class="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-3">
                                        Edit
                                     </a>
+                                     <form method="POST" action="{{ route('skills.destroy', $skill->id) }}" class="mb-0">
+                                        @csrf
+                                        @method('DELETE')
+                                        <a href="{{ route('skills.destroy', $skill->id) }}" 
+                                            onclick="event.preventDefault(); this.closest('form').submit();"
+                                            class="font-medium text-red-600 dark:text-red-500 hover:underline mr-6">
+                                            Delete
+                                         </a>
+                                    </form>
                                 </td>
                             </tr>
                         @empty
@@ -58,3 +82,12 @@
         </div>
     </div>
 </x-app-layout>
+
+<script type="module">
+
+    $('#targetElement').click(function(e) {
+
+        $('#triggerElement').hide('slow');
+    })
+
+</script>
